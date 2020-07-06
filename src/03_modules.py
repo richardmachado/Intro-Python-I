02cd6529fe45925b9abeed752a84665fc6d21bcc
Lambda_School_Loaner_198 <richardmachado@Lambda-School-Loaner-198s-MacBook-Pro.local>
2020-07-06 08:02:58 -0700

    saving work

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,12 +11,18 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
+# print sys.argv[]
+
+
+
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print (sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
 
+print (sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
@@ -24,8 +30,13 @@ import os
 # Print the current process ID
 # YOUR CODE HERE
 
+pid=0
+print(os.getpgid(pid))
+
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+
+print(os.getcwd)
 
 # Print out your machine's login name
 # YOUR CODE HERE
