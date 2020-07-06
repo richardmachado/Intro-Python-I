f93a4b516dd50cdbc474039d1dc9067f40d442ea
Richard Machado <mmachado.richard@gmail.com>
2020-07-06 12:28:36 -0700

    stuck on 04

--- src/03_modules.py
+++ src/03_modules.py
@@ -17,12 +17,12 @@ import sys
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-print (sys.platform)
+print(sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
 
-print (sys.version)
+print(sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
@@ -30,13 +30,14 @@ import os
 # Print the current process ID
 # YOUR CODE HERE
 
-pid=0
-print(os.getpgid(pid))
+# pid=0
+# print(os.getpgid(pid))
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
 
-print(os.getcwd)
+print(os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print(os.getlogin())
